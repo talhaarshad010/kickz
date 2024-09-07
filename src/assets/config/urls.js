b4426bf34d1b2a1d6415fb609820f7cc11550210
MUHAMMAD TALHA ARSHAD <talhaarshad010@gmail.com>
2024-09-07 05:17:58 +0500

    5:17

--- src/assets/config/urls.js
+++ src/assets/config/urls.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http:// 192.168.1.102:3000';
+export const API_BASE_URL = 'http://192.168.2.102:3000';
 export const PRODUCTS_API_BASE_URL = 'https://dummyjson.com';
 // export const API_BASE_URL = 'http://192.168.150.238:3000';
 export const getAPiurl = endpoint => API_BASE_URL + endpoint;
